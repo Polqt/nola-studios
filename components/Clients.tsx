1f6814e5fd9f1eefbcba3975373c0a0771a82c6e
Polqt <poyhidalgo@gmail.com>
2025-04-19 13:43:42 +0800

    chore: add Clients and Services components for improved client and service representation

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,7 +1,48 @@
 export default function Clients() {
+  const clients = [
+    {
+      id: 1,
+      name: 'FOINE Essence',
+      imageSrc: '',
+    },
+    {
+      id: 2,
+      name: 'Jani Essence',
+      imageSrc: '',
+    },
+    {
+      id: 3,
+      name: 'Winjan Essence',
+      imageSrc: '',
+    },
+    {
+      id: 3,
+      name: 'Winjan Essence',
+      imageSrc: '',
+    },
+    {
+      id: 3,
+      name: 'Winjan Essence',
+      imageSrc: '',
+    },
+  ];
+
   return (
-    <div className="flex flex-row justify-center items-center mb-4">
-      <h1 className="text-6xl font-bold">our wonderful clients</h1>
+    <div className="w-full container mx-auto px-4 md:px-2 lg:px-8 py-32 md:py-48">
+      <div className="flex flex-col items-center">
+        <h1 className="font-bold text-5xl md:text-6xl lg:text-7xl mb-32 text-center">
+          our wonderful clients
+        </h1>
+
+        <div className="flex justify-center items-center gap-6 md:gap-12 lg:gap-16">
+          {clients.map(client => (
+            <div key={client.id}>
+              <div className="w-40 h-40 md:w-32 md:h-32 bg-black rounded-full" />
+              <span className="sr-only">{client.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- components/Services.tsx
+++ components/Services.tsx
@@ -1,21 +1,21 @@
-import { BookOpen, Pencil, Play } from 'lucide-react';
+import { BookOpen, FastForward, Paintbrush } from 'lucide-react';
 
 export default function Services() {
   const services = [
     {
-      icon: <Pencil size={32} />,
+      icon: <Paintbrush size={48} fill="currentColor" strokeWidth={0} />,
       title: 'we shape your vision',
       description:
         'from articulating your campaign vision, we bring your ideas to life with purpose and originality',
     },
     {
-      icon: <BookOpen size={32} />,
+      icon: <BookOpen size={48} fill="currentColor" strokeWidth={0} />,
       title: 'we create your story',
       description:
         'high-impact video, photography, and design that capture attention and connect with your audience.',
     },
     {
-      icon: <Play size={32} />,
+      icon: <FastForward size={48} fill="currentColor" strokeWidth={0} />,
       title: 'we amplify your reach',
       description:
         'from social media to paid ads, we turn your message into momentum- viral is just the beginning.',
@@ -24,7 +24,7 @@ export default function Services() {
   return (
     <div className="w-full container mx-auto px-4 md:px-6 lg:px-8 py-16 md:py-24">
       <div className="flex flex-col justify-center items-center mb-12 md:mb-16 text-center">
-        <h1 className="font-bold text-4xl md:Text-5xl lg:text-6xl mb-4">
+        <h1 className="font-bold text-4xl md:text-5xl lg:text-6xl mb-4">
           what can we do for you
         </h1>
         <p className="text-lg md:text-xl max-w-2xl">
