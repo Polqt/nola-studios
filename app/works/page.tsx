eb3ec8c715367770492ce1d08bef1b0784c821a2
Polqt <poyhidalgo@gmail.com>
2025-05-03 01:32:06 +0800

    refactor: remove unused mouse position and cursor variant state management in WorkPage

--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,39 +1,38 @@
 'use client';
 
 import BackgroundParticles from '@/components/BackgroundParticles';
-import CustomCursor from '@/components/CustomCursor';
+// import CustomCursor from '@/components/CustomCursor';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
-import { useEffect, useState } from 'react';
 
 export default function WorkPage() {
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-  const [cursorVariant] = useState<'default' | 'text'>('default');
+  // const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  // const [cursorVariant] = useState<'default' | 'text'>('default');
 
-  useEffect(() => {
-    const mouseMove = (e: MouseEvent) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      });
-    };
+  // useEffect(() => {
+  //   const mouseMove = (e: MouseEvent) => {
+  //     setMousePosition({
+  //       x: e.clientX,
+  //       y: e.clientY,
+  //     });
+  //   };
 
-    window.addEventListener('mousemove', mouseMove);
+  //   window.addEventListener('mousemove', mouseMove);
 
-    return () => {
-      window.removeEventListener('mousemove', mouseMove);
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('mousemove', mouseMove);
+  //   };
+  // }, []);
 
   // const textHoverEnter = () => setCursorVariant('text');
   // const textHoverLeave = () => setCursorVariant('default');
 
   return (
     <div className="min-h-screen flex flex-col bg-neutral-900 text-white overflow-hidden relative">
-      <CustomCursor
+      {/* <CustomCursor
         mousePosition={mousePosition}
         cursorVariant={cursorVariant}
-      />
+      /> */}
       <BackgroundParticles />
 
       <div className="flex-grow relative z-10">
